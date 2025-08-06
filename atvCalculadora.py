6df140bf0fbba392b9666c3f13fa81cf41f47489
Caique <fn7.dzn@gmail.com>
2025-08-06 20:29:39 -0300

    Att Calculadora

--- atvCalculadora.py
+++ atvCalculadora.py
@@ -1,5 +1,5 @@
-op = True
-while op:
+cond = True
+while cond:
     n1_str = input('digite o primeiro número: ')
     n2_str = input('digite o segundo número: ')
 
@@ -10,4 +10,19 @@ while op:
         print('você digitou números inválidos.')
         continue
     
-    input('qual operação matemática você deseja realizar? / 1. Adição / ')
+    op = input('qual operação matemática você deseja realizar?\n 1. Adiçâo\n 2. Subtração\n 3. Multiplicação\n 4. Divisão\n')
+
+    if op == '1':
+        print(f'a soma entre {n1_float} e {n2_float} é {n1_float + n2_float}')
+    elif op == '2':
+        print(f'a subtração entre {n1_float} e {n2_float} é {n1_float - n2_float}')
+    elif op == '3':
+        print(f'a multiplicação entre {n1_float} e {n2_float} é {n1_float * n2_float}')
+    elif op == '4':
+        if n2_float == 0:
+            print('divisão impossivel por 0. digite os numeros novamente.')
+            continue
+        else:
+            print(f'a divisão entre {n1_float} e {n2_float} é {n1_float / n2_float}')
+    else:
+        print('você não digitou nenhuma opção valida')
